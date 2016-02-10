6de9fac4a35fdf9f0b7e89a05581422b63a4afc3
James Ko <jamesqko@gmail.com>
2016-02-10 00:20:53 -0500

    (API) Add generic commands

--- src/Typed.Xaml/Commands/Command.cs
+++ src/Typed.Xaml/Commands/Command.cs
@@ -2,10 +2,41 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.Windows.Input;
 
 namespace Typed.Xaml
 {
-    public class Command
+    public class Command : ICommand
     {
+        private readonly Action execute;
+
+        public event EventHandler CanExecuteChanged;
+
+        public Command(Action execute)
+        {
+            this.execute = execute;
+        }
+
+        public bool CanExecute(object parameter) => true;
+
+        public void Execute(object parameter) => execute();
+    }
+
+    public class Command<T> : ICommand
+    {
+        private readonly Action<T> execute;
+
+        public event EventHandler CanExecuteChanged;
+
+        public Command(Action<T> execute)
+        {
+            this.execute = execute;
+        }
+
+        public bool CanExecute(object parameter) =>
+            parameter is T;
+
+        public void Execute(object parameter) =>
+            execute((T)parameter);
     }
 }
