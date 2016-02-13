efb065bcec5b3c6059d28cb03e7636ab9f50121f
James Ko <jamesqko@gmail.com>
2016-02-13 14:47:28 -0500

    (API) Add FrameExtensions for netcore50

---
+++ src/Typed.Xaml/FrameExtensions.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Media.Animation;
+
+namespace Typed.Xaml
+{
+    public static class FrameExtensions
+    {
+#if NETCORE50
+        public static bool Navigate<T>(this Frame frame) =>
+            frame.Navigate(typeof(T));
+
+        public static bool Navigate<T>(this Frame frame, object parameter) =>
+            frame.Navigate(typeof(T), parameter);
+
+        public static bool Navigate<T>(this Frame frame, object parameter, NavigationTransitionInfo infoOverride) =>
+            frame.Navigate(typeof(T), parameter, infoOverride);
+#endif
+    }
+}
