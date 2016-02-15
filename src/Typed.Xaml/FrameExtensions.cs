0d616d876b93f1adfd58e193f11cc6c014bf631d
James Ko <jamesqko@gmail.com>
2016-02-15 17:51:38 -0500

    (API) Clean code counts

--- src/Typed.Xaml/FrameExtensions.cs
+++ src/Typed.Xaml/FrameExtensions.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Controls;
@@ -8,25 +9,17 @@ using Windows.UI.Xaml.Media.Animation;
 
 namespace Typed.Xaml
 {
+#if NETCORE50
     public static class FrameExtensions
     {
         public static bool Navigate<T>(this Frame frame) =>
-#if NET46
-            frame.Navigate(Activator.CreateInstance<T>());
-#elif NETCORE50
             frame.Navigate(typeof(T));
-#endif
 
         public static bool Navigate<T>(this Frame frame, object parameter) =>
-#if NET46
             frame.Navigate(typeof(T), parameter);
-#elif NETCORE50
-            frame.Navigate(Activator.CreateInstance<T>(), parameter);
-#endif
 
-#if NETCORE50
         public static bool Navigate<T>(this Frame frame, object parameter, NavigationTransitionInfo infoOverride) =>
             frame.Navigate(typeof(T), parameter, infoOverride);
-#endif
     }
+#endif
 }
