2cc7909c786213f06bc3d03e96d12352e8a14e9a
James Ko <jamesqko@gmail.com>
2016-02-15 23:32:48 -0500

    Fix dead code in FrameExtensions

--- src/Typed.Xaml/FrameExtensions.cs
+++ src/Typed.Xaml/FrameExtensions.cs
@@ -8,7 +8,7 @@ using Windows.UI.Xaml.Media.Animation;
 
 namespace Typed.Xaml
 {
-#if NETCORE50
+#if !NET451
     public static class FrameExtensions
     {
         public static bool Navigate<T>(this Frame frame) =>
