8682c611a95f183a149f0c83befe37c065553e9d
James Ko <jamesqko@gmail.com>
2016-02-15 22:42:26 -0500

    React to Shims.Xaml changes: target net451

--- src/Typed.Xaml/Converters/Converter.cs
+++ src/Typed.Xaml/Converters/Converter.cs
@@ -39,7 +39,7 @@ namespace Typed.Xaml.Converters
 
         // Different APIs: WPF uses Convert(object, Type, object, CultureInfo),
         // while WinRT has Convert(object, Type, object, string).
-#if NET46
+#if NET451
         object IValueConverter.Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
             return Convert(value, targetType, parameter, culture);
