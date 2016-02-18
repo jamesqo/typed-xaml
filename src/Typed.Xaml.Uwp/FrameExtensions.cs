51b869da8bf1ffe6d58f519fa490f3e8d532813c
James Ko <jamesqko@gmail.com>
2016-02-18 14:53:10 -0500

    (API) Enforce a type constraint on Page for Frame.Navigate

--- src/Typed.Xaml.Uwp/FrameExtensions.cs
+++ src/Typed.Xaml.Uwp/FrameExtensions.cs
@@ -8,13 +8,22 @@ namespace Typed.Xaml
 {
     public static class FrameExtensions
     {
-        public static bool Navigate<T>(this Frame frame) =>
-            frame.Navigate(typeof(T));
+        public static bool Navigate<T>(this Frame frame)
+            where T : Page
+        {
+            return frame.Navigate(typeof(T));
+        }
 
-        public static bool Navigate<T>(this Frame frame, object parameter) =>
-            frame.Navigate(typeof(T), parameter);
+        public static bool Navigate<T>(this Frame frame, object parameter)
+            where T : Page
+        {
+            return frame.Navigate(typeof(T), parameter);
+        }
 
-        public static bool Navigate<T>(this Frame frame, object parameter, NavigationTransitionInfo infoOverride) =>
-            frame.Navigate(typeof(T), parameter, infoOverride);
+        public static bool Navigate<T>(this Frame frame, object parameter, NavigationTransitionInfo infoOverride)
+            where T : Page
+        {
+            return frame.Navigate(typeof(T), parameter, infoOverride);
+        }
     }
 }
