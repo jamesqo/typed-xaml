cb0b994b7615e1e8461f66971c42d422607a1562
James Ko <jamesqko@gmail.com>
2016-02-18 01:49:59 -0500

    Update publish.cmd

--- publish.cmd
+++ publish.cmd
@@ -1,6 +1,43 @@
 @echo off
 setlocal
 
+:: Parse arguments
+:arg_loop
+
+if "%~1" == "" goto args_done
+
+if /i "%~1" == "-h" goto usage
+if /i "%~1" == "-?" goto usage
+if /i "%~1" == "--help" goto usage
+
+if /i "%~1" == "-v" (
+    set "version=%~2"
+    shift
+)
+
+if /i "%~1" == "--version" (
+    set "version=%~2"
+    shift
+)
+
+shift
+goto arg_loop
+
+:usage
+
+echo Usage:
+echo ./publish [option]...
+echo.
+echo Options:
+echo -v,--version VERSION        Set the package's version to VERSION
+
+goto :eof
+
+:args_done
+
+:: Version is required
+if not defined version goto usage
+
 :: Restore NuGet
 set "nugetdir=%~dp0bin"
 set "nuget=%nugetdir%\nuget.exe"
@@ -16,13 +53,9 @@ if not exist "%nuget%" (
 :: Build the solution
 call "%~dp0build.cmd" -p "Any CPU" -c Release
 
-:: Push the packages
-set "bin=%~dp0artifacts\bin"
-cd "%bin%"
-for /d %%d in (*) do (
-    cd "%%d\Release"
-    del /q *.symbols.nupkg
-    "%nuget%" push *.nupkg
-    del /q *.nupkg > nul
-    cd "%bin%"
-)
+:: Create the package
+cd "%~dp0src"
+del /q *.nupkg > nul 2>&1
+"%nuget%" pack *.nuspec -Prop Version=%version%
+"%nuget%" push *.nupkg
+del /q *.nupkg > nul
