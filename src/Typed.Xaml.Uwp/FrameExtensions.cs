89f724e8c36d271f1c5ee4b978b87b776321b95d
James Ko <jamesqko@gmail.com>
2016-02-17 20:46:08 -0500

    Move FrameExtensions to the UWP sub-project

---
+++ src/Typed.Xaml.Uwp/FrameExtensions.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Media.Animation;
+
+namespace Typed.Xaml
+{
+    public static class FrameExtensions
+    {
+        public static bool Navigate<T>(this Frame frame) =>
+            frame.Navigate(typeof(T));
+
+        public static bool Navigate<T>(this Frame frame, object parameter) =>
+            frame.Navigate(typeof(T), parameter);
+
+        public static bool Navigate<T>(this Frame frame, object parameter, NavigationTransitionInfo infoOverride) =>
+            frame.Navigate(typeof(T), parameter, infoOverride);
+    }
+}
