974edcaebe1024360a504574ce257e19078dab98
James Ko <jamesqko@gmail.com>
2016-02-17 20:55:57 -0500

    Remove the #ifs and introduce conditional compilation

---
+++ src/Typed.Xaml.Uwp/Converters/ConverterBase.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml.Data;
+
+namespace Typed.Xaml.Converters
+{
+    public abstract partial class ConverterBase : IValueConverter
+    {
+        object IValueConverter.Convert(object value, Type targetType, object parameter, string language)
+        {
+            return this.Convert(value, targetType, parameter, new CultureInfo(language));
+        }
+
+        object IValueConverter.ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            return this.ConvertBack(value, targetType, parameter, new CultureInfo(language));
+        }
+    }
+}
