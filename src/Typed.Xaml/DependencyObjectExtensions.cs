ccae11936e2614ef911d22cce58d54360a5f2130
James Ko <jamesqko@gmail.com>
2016-02-21 00:04:12 -0500

    (API) Add ReadLocal/TryReadLocal extension methods

--- src/Typed.Xaml/DependencyObjectExtensions.cs
+++ src/Typed.Xaml/DependencyObjectExtensions.cs
@@ -11,10 +11,31 @@ namespace Typed.Xaml
         {
             return (T)obj.GetValue(property);
         }
+        
+        public static T ReadLocal<T>(this DependencyObject obj, DependencyProperty property)
+        {
+            // TODO: Handle checking for DependencyProperty.UnsetValue.
+            return (T)obj.ReadLocalValue(property);
+        }
 
         public static void Set<T>(this DependencyObject obj, DependencyProperty property, T value)
         {
             obj.SetValue(property, value);
+        }
+        
+        public static bool TryReadLocal<T>(this DependencyObject obj, DependencyProperty property, out T value)
+        {
+            // TODO: Do we need to handle DependencyProperty.UnsetValue?
+            object result = obj.ReadLocalValue(property);
+            
+            if (result is T)
+            {
+                value = (T)result;
+                return true;
+            }
+            
+            value = default(T);
+            return false;
         }
     }
 }
